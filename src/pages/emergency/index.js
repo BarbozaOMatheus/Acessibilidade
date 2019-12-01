0780ec50cae553eeec3816b4dceab09c7c125651
wild santos barreto <wildsbsrreto@gmail.com>
2019-12-01 18:43:15 -0300

    Tela de contatos de emergencia

--- src/pages/emergency/index.js
+++ src/pages/emergency/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-
 import * as RNFS from 'react-native-fs'
-import { View, TextInput, TouchableOpacity } from "react-native";
+
+import {
+  View, Text, StyleSheet, Platform, PermissionsAndroid,
+  Linking,
+  Alert, FlatList, TextInput, TouchableOpacity
+} from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Contacts from 'react-native-contacts';
+import ContactsWrapper from 'react-native-contacts-wrapper';
 
 import styles from './estilo';
 
@@ -40,9 +46,44 @@ export default class Sobre extends Component {
     this.setState({ name: aux[1] })
     this.setState({ phoneNumber: aux[2] })
   }
+  onButtonPressed = () => (
 
+    ContactsWrapper.getContact()
+      .then((contact) => {
+        console.log(contact)
+      })
+      .catch((error) => {
+        console.log("ERROR CODE: ", error.code);
+        console.log("ERROR MESSAGE: ", error.message);
+      })
+  );
 
+  componentDidMount() {
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+        {
+          title: 'Contacts',
+          message: ' This app would like to see your contacts'
+        }
+      ).then(() => {
+        this.getList();
+      })
+    } else if (Platform.OS === 'ios') {
+      this.getList();
+    }
+  }
 
+  getList = () => {
+    Contacts.getAll((err, contacts) => {
+      if (err === 'denied') {
+        console.log("cannot access");
+      } else {
+        this.setState({ contacts });
+        console.log(contacts);
+      }
+    })
+  }
 
   render() {
     return (
@@ -57,9 +98,27 @@ export default class Sobre extends Component {
 
         </View>
 
-        <View style={styles.center}></View>
-
+        <View style={styles.center}>
+        </View>
         <View style={styles.bottom}>
+          <View style={{ height: '10%', }}>
+            <Text style={styles.texto}>Contato de emergência:</Text>
+            <Text style={styles.texto}>{this.state.name = "Police"}</Text>
+            <Text style={styles.texto}>{this.state.phoneNumber = "8938349"}</Text>
+          </View>
+          <View style={{ height: '60%', }}>
+            <Text style={styles.texto}></Text>
+          </View>
+
+          <View style={styles.menu}>
+            <View style={styles.bottomItem}>
+              <TouchableOpacity style={styles.bottomItemInner}
+                onPress={() => this.onButtonPressed()}
+              >
+                <Text style={styles.texto2}>Escolher Contato</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
 
         </View>
 
@@ -67,4 +126,3 @@ export default class Sobre extends Component {
     );
   }
 }
-
